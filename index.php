9acb79c7f5410ef623bd35126afe3547aecfb2f6
Nguyễn Văn Vửng <130837811+VungNguyenYT@users.noreply.github.com>
2024-11-12 16:25:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,15 @@ include("header.php");
 include("ketnoi.php");
 $sql = "select* from sach";
 $kq = mysqli_query($fcn,$sql);
-echo("<table border='0' width='100%'>"); $i=0
+echo("<table border='0' width='100%'>"); $i=0;
 while($row=mysqli_fetch_array($kq)){
-    echo("<tr>");
-    echo("<td><img src='".$row["hinhsach"]."' width='90' height='90'> </td>");
-    echo("<td> Tên sách:".$row["tensach"]."<br> Tác giả:".$row["tacgia"]."<br> </td>");
-    echo("</tr>");
+    if($i%2==0)
+        echo("<tr>");
+    echo("<td><img src='".$row["HINH_SACH"]."' width='90' height='90'> </td>");
+    echo("<td> Tên sách:".$row["TEN_SACH"]."<br> Tác giả:".$row["TAC_GIA"]."<br> </td>");
+    if($i%2!=0)
+        echo("</tr>");
+    $i++;
 }
 echo("</table>");
 ?>
