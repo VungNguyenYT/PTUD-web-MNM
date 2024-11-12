d76ba4bbec1b589cf05826fd54338dd33b45f564
Nguyễn Văn Vửng <130837811+VungNguyenYT@users.noreply.github.com>
2024-11-12 15:46:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 include("header.php");
 ?>
-giao diện chính
+ 
+<?php
+include("ketnoi.php");
+$sql = "select* from sach";
+$kq = mysqli_query($sfcn,$sql);
+echo("<table border='0' width='100%'>");
+while($row=mysqli_fetch_array($kq)){
+    echo("<tr>");
+    echo("<td><img src='".$row["hinhsach"]."' width='90' height='90'> </td>");
+    echo("<td> Tên sách:".$row["tensach"]."<br> Tác giả:".$row["tacgia"]."<br> </td>");
+    echo("</tr>");
+}
+echo("</table>");
+?>
+
 <?php
 include("footer.php");
 ?>
